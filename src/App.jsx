200c2c1d118d78afaa00c1e4809538aeadc55eca
Periklis <perik76@gmail.com>
2024-04-25 00:08:17 +0300

    Add cheack read functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,33 @@ function App() {
   const [emails, setEmails] = useState(initialEmails)
 
 
-  // console.log(initialEmails)
+ 
 
-function handleEmailDisplay(emails) {
-  return emails.map((email, index) => (
-    <li className="email" key={index}>
-      <div className="select">
-        <input className="select-checkbox" type="checkbox" />
-      </div>
-      <div className="star">
-        <input className="star-checkbox" type="checkbox" />
-      </div>
-      <div className="sender">{email.sender}</div>
-      <div className="title">{email.title}</div>
-    </li>
-  ));
+  function handleEmailDisplay(emails) {
+    return emails.map((email, index) => (
+      <li className="email" key={index}>
+        <div className="select">
+          <input className="select-checkbox" type="checkbox"
+            checked={email.read} onChange={() => handleToggleRead(index)}
+          />
+        </div>
+        <div className="star">
+          <input className="star-checkbox" type="checkbox" />
+        </div>
+        <div className="sender">{email.sender}</div>
+        <div className="title">{email.title}</div>
+      </li>
+    ));
   }
-  
-  
+
+  function handleToggleRead(index) {
+    const updatedEmails = [...emails]; 
+    updatedEmails[index].read = !updatedEmails[index].read;
+    setEmails(updatedEmails); 
+  }
+   console.log(initialEmails)
+
+
   return (
     <div className="app">
       <Header />
